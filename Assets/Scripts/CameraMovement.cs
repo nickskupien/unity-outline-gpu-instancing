27828675894f5ae7d09535b75b3184a5a2cf0419
Nicholas Skupien <nicholasskupien@gmail.com>
2021-07-13 01:34:54 -0400

    next thing is to improve camera controls

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -10,6 +10,7 @@ public class CameraMovement : MonoBehaviour
     [Range(0f, 10f)]
     public float panSpeed = 3;
 
+    public float pitch = 30;
     private Vector2 orbitAngles = new Vector2(30,45f);
     private Vector3 focusPoint = new Vector3(0,10,0);
 
@@ -25,6 +26,7 @@ public class CameraMovement : MonoBehaviour
     void LateUpdate()
     {
         orbitAngles.y += Input.GetAxis("Mouse X") * rotationSpeed * Time.unscaledDeltaTime * Input.GetAxis("Fire1");
+        orbitAngles.x = pitch;
         posY += Input.GetAxis("Vertical") * panSpeed * Time.unscaledDeltaTime;
         Quaternion lookRotation = Quaternion.Euler(orbitAngles);
         Vector3 lookDirection = lookRotation * Vector3.forward;
